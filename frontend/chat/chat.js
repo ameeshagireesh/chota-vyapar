bef3e3e2ecebe0ae5072ba9a55172a7a34c8791f
ameeshagireesh <ameesha.gireeshkm@gmail.com>
2022-11-09 22:30:18 +0530

    fix: code cleanup

--- frontend/chat/chat.js
+++ frontend/chat/chat.js
@@ -4,34 +4,36 @@ const msgerChat = get(".msger-chat");
 const BOT_IMG = "bot.png";
 const PERSON_IMG = "user.png";
 const BOT_NAME = "BOT";
-const PERSON_NAME = "Coderider";
+const PERSON_NAME = "Customer";
+
 const prompts = [
   ["hi", "hey", "hello", "good morning", "good afternoon"],
   ["how are you", "how is life", "how are things"],
-  ["what are you doing", "what is going on", "what is up"],
-  ["how old are you"],
-  ["who are you", "are you human", "are you bot", "are you human or bot"],
-  ["who created you", "who made you"],
-  [
-    "your name please",
-    "your name",
-    "may i know your name",
-    "what is your name",
-    "what call yourself"
-  ],
-  ["i love you"],
-  ["happy", "good", "fun", "wonderful", "fantastic", "cool"],
-  ["bad", "bored", "tired"],
-  ["help me", "tell me story", "tell me joke"],
-  ["ah", "yes", "ok", "okay", "nice"],
-  ["bye", "good bye", "goodbye", "see you later"],
-  ["what should i eat today"],
-  ["bro"],
-  ["what", "why", "how", "where", "when"],
-  ["no", "not sure", "maybe", "no thanks"],
-  [""],
-  ["haha", "ha", "lol", "hehe", "funny", "joke"]
+//   ["what are you doing", "what is going on", "what is up"],
+//   ["how old are you"],
+//   ["who are you", "are you human", "are you bot", "are you human or bot"],
+//   ["who created you", "who made you"],
+//   [
+//     "your name please",
+//     "your name",
+//     "may i know your name",
+//     "what is your name",
+//     "what call yourself"
+//   ],
+//   ["i love you"],
+//   ["happy", "good", "fun", "wonderful", "fantastic", "cool"],
+//   ["bad", "bored", "tired"],
+//   ["help me", "tell me story", "tell me joke"],
+  ["what do you sell"],
+  ["i want to buy"],
+  ["scrunchies"],
+  ["wallets"],
+  ["jewellery"],
+  ["clothes"],
+  ["bath products"],
+  ["desserts"],
 ]
+
 const replies = [
   ["Hello!", "Hi!", "Hey!", "Hi there!", "Howdy"],
   [
@@ -39,29 +41,30 @@ const replies = [
     "Pretty well, how are you?",
     "Fantastic, how are you?"
   ],
-  [
-    "Nothing much",
-    "About to go to sleep",
-    "Can you guess?",
-    "I don't know actually"
-  ],
-  ["I am infinite"],
-  ["I am just a bot", "I am a bot. What are you?"],
-  ["The one true God, JavaScript"],
-  ["I am nameless", "I don't have a name"],
-  ["I love you too", "Me too"],
-  ["Have you ever felt bad?", "Glad to hear it"],
-  ["Why?", "Why? You shouldn't!", "Try watching TV"],
-  ["What about?", "Once upon a time..."],
-  ["Tell me a story", "Tell me a joke", "Tell me about yourself"],
-  ["Bye", "Goodbye", "See you later"],
-  ["Sushi", "Pizza"],
-  ["Bro!"],
-  ["Great question"],
-  ["That's ok", "I understand", "What do you want to talk about?"],
-  ["Please say something :("],
-  ["Haha!", "Good one!"]
+//   [
+//     "Nothing much",
+//     "About to go to sleep",
+//     "Can you guess?",
+//     "I don't know actually"
+//   ],
+//   ["I am infinite"],
+//   ["I am just a bot", "I am a bot. What are you?"],
+//   ["The one true God, JavaScript"],
+//   ["I am nameless", "I don't have a name"],
+//   ["I love you too", "Me too"],
+//   ["Have you ever felt bad?", "Glad to hear it"],
+//   ["Why?", "Why? You shouldn't!", "Try watching TV"],
+//   ["What about?", "Once upon a time..."],
+  ["Scrunchies, Desserts, Wallets, Jewellery, Clothes, Bath products"],
+  ["what do you want to buy"],
+  ["@handmadescrunchie_"],
+  ["@haveitbagit", "@thetrendmakers", "@bombaythreads"],
+  ["@cutiefy", "@unimaginableprices"],
+  ["@femella", "@shopxanomaly", "@artisanluxe", "@rozedo_jaipur"],
+  ["@soapaffairs", "@foamandfizzsoaps"],
+  ["@thewhiskcorner", "@thechibicakeshop", "@sugarfanciesbypooja"],
 ];
+
 const alternative = [
   "Same",
   "Go on...",
@@ -70,7 +73,9 @@ const alternative = [
   "I'm listening...",
   "I don't understand :/"
 ]
+
 const robot = ["How do you do, fellow human", "I am not a bot"];
+
 msgerForm.addEventListener("submit", event => {
   event.preventDefault();
   const msgText = msgerInput.value;
